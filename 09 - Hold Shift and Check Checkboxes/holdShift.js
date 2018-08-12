19999f9393552ed0574498deb5ccfa86a7b74d5a
Andres Outon <outon.x@gmail.com>
2018-08-11 23:35:26 -0500

    added shift-select functionality to list of checkboxes

---
+++ 09 - Hold Shift and Check Checkboxes/holdShift.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+  //check if they are pressing shift
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    //loop over checkboxes
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
