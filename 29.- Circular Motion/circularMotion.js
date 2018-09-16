b170d47e073e246f064ae7e24ab917a814c36112
Andres Outon <outon.x@gmail.com>
2018-09-15 23:59:58 -0500

    created motion

--- 29.- Circular Motion/circularMotion.js
+++ 29.- Circular Motion/circularMotion.js
@@ -41,33 +41,42 @@ addEventListener("resize", () => {
 });
 
 // Objects
-function Object(x, y, radius, color) {
+function Particle(x, y, radius, color) {
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.color = color;
+  this.radians = 0;
+  this.velocity = 0.05;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  };
+
+  this.update = function() {
+    //Move points over time
+    this.radians += this.velocity;
+    this.x = x + Math.cos(this.radians) * 100;
+    this.y = y + Math.sin(this.radians) * 100;
+    this.draw();
+  };
 }
 
-Object.prototype.draw = function() {
-  c.beginPath();
-  c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-  c.fillStyle = this.color;
-  c.fill();
-  c.closePath();
-};
-
-Object.prototype.update = function() {
-  this.draw();
-};
-
 // Implementation
-let objects;
+let particles;
 function init() {
-  objects = [];
+  particles = [];
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push();
+  for (let i = 0; i < 1; i++) {
+    particles.push(
+      new Particle(canvas.width / 2, canvas.height / 2, 5, "blue")
+    );
   }
+  console.log(particles);
 }
 
 // Animation Loop
@@ -75,10 +84,9 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
-  c.fillText("HTML CANVAS BOILERPLATE", mouse.x, mouse.y);
-  // objects.forEach(object => {
-  //  object.update();
-  // });
+  particles.forEach(particle => {
+    particle.update();
+  });
 }
 
 init();
